d88bc8d7f5a96bfbf533e90c4ac0c109b763efef
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:16:40 -0800

    Add files via upload

---
+++ Alien_Colors_2.ts
@@ -0,0 +1,23 @@
+//Written by :TAYYABA RAO 
+//Date; 18/feb/2024
+
+//color set
+let alien_color :string = 'green';
+
+if (alien_color ==='green'){
+    console.log("layers just earnd 5 Rupee! for shooting the alien_color.");
+
+}
+else
+{
+    console.log("Players just earned 10 Rupee!");
+}
+ 
+//change color atleast get 10 rupee
+alien_color = 'yellow';
+
+if (alien_color ==='green'){
+    console.log("Players just earned 5 Rupee for shooting the alien!");
+}
+// if we want to biscits wo make it faksr for rs10
+console.log("Players just earned 10 Rupee!");
